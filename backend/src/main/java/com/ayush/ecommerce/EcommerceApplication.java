34f5a60b045e92ddb679659ada0c73dc072a98e3
Ayush Seth <er.ayushseth@gmail.com>
2026-06-22 17:10:23 +0530

    trying to fix github profile conflicts, added comment to main file

--- backend/src/main/java/com/ayush/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/ayush/ecommerce/EcommerceApplication.java
@@ -10,5 +10,6 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+	//fixing GitHub profile conflicts
 
 }
